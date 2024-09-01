1e261edfb87691aa50c3a86609ae89b7c3e543b0
Nikola Bukov <paradoxxxbg69@gmail.com>
2024-09-01 19:45:19 +0300

    Added some stuff

---
+++ бръх.cpp
@@ -0,0 +1,273 @@
+bool isEven(int a) {
+    if (a == 0) {
+        return true;
+    } else if (a == 1) {
+        return false;
+    } else if (a == 2) {
+        return true;
+    } else if (a == 3) {
+        return false;
+    } else if (a == 4) {
+        return true;
+    } else if (a == 5) {
+        return false;
+    } else if (a == 6) {
+        return true;
+    } else if (a == 7) {
+        return false;
+    } else if (a == 8) {
+        return true;
+    } else if (a == 9) {
+        return false;
+    } else if (a == 10) {
+        return true;
+    } else if (a == 11) {
+        return false;
+    } else if (a == 12) {
+        return true;
+    } else if (a == 13) {
+        return false;
+    } else if (a == 14) {
+        return true;
+    } else if (a == 15) {
+        return false;
+    } else if (a == 16) {
+        return true;
+    } else if (a == 17) {
+        return false;
+    } else if (a == 18) {
+        return true;
+    } else if (a == 19) {
+        return false;
+    } else if (a == 20) {
+        return true;
+    } else if (a == 21) {
+        return false;
+    } else if (a == 22) {
+        return true;
+    } else if (a == 23) {
+        return false;
+    } else if (a == 24) {
+        return true;
+    } else if (a == 25) {
+        return false;
+    } else if (a == 26) {
+        return true;
+    } else if (a == 27) {
+        return false;
+    } else if (a == 28) {
+        return true;
+    } else if (a == 29) {
+        return false;
+    } else if (a == 30) {
+        return true;
+    } else if (a == 31) {
+        return false;
+    } else if (a == 32) {
+        return true;
+    } else if (a == 33) {
+        return false;
+    } else if (a == 34) {
+        return true;
+    } else if (a == 35) {
+        return false;
+    } else if (a == 36) {
+        return true;
+    } else if (a == 37) {
+        return false;
+    } else if (a == 38) {
+        return true;
+    } else if (a == 39) {
+        return false;
+    } else if (a == 40) {
+        return true;
+    } else if (a == 41) {
+        return false;
+    } else if (a == 42) {
+        return true;
+    } else if (a == 43) {
+        return false;
+    } else if (a == 44) {
+        return true;
+    } else if (a == 45) {
+        return false;
+    } else if (a == 46) {
+        return true;
+    } else if (a == 47) {
+        return false;
+    } else if (a == 48) {
+        return true;
+    } else if (a == 49) {
+        return false;
+    } else if (a == 50) {
+        return true;
+    } else if (a == 51) {
+        return false;
+    } else if (a == 52) {
+        return true;
+    } else if (a == 53) {
+        return false;
+    } else if (a == 54) {
+        return true;
+    } else if (a == 55) {
+        return false;
+    } else if (a == 56) {
+        return true;
+    } else if (a == 57) {
+        return false;
+    } else if (a == 58) {
+        return true;
+    } else if (a == 59) {
+        return false;
+    } else if (a == 60) {
+        return true;
+    } else if (a == 61) {
+        return false;
+    } else if (a == 62) {
+        return true;
+    } else if (a == 63) {
+        return false;
+    } else if (a == 64) {
+        return true;
+    } else if (a == 65) {
+        return false;
+    } else if (a == 66) {
+        return true;
+    } else if (a == 67) {
+        return false;
+    } else if (a == 68) {
+        return true;
+    } else if (a == 69) {
+        return false;
+    } else if (a == 70) {
+        return true;
+    } else if (a == 71) {
+        return false;
+    } else if (a == 72) {
+        return true;
+    } else if (a == 73) {
+        return false;
+    } else if (a == 74) {
+        return true;
+    } else if (a == 75) {
+        return false;
+    } else if (a == 76) {
+        return true;
+    } else if (a == 77) {
+        return false;
+    } else if (a == 78) {
+        return true;
+    } else if (a == 79) {
+        return false;
+    } else if (a == 80) {
+        return true;
+    } else if (a == 81) {
+        return false;
+    } else if (a == 82) {
+        return true;
+    } else if (a == 83) {
+        return false;
+    } else if (a == 84) {
+        return true;
+    } else if (a == 85) {
+        return false;
+    } else if (a == 86) {
+        return true;
+    } else if (a == 87) {
+        return false;
+    } else if (a == 88) {
+        return true;
+    } else if (a == 89) {
+        return false;
+    } else if (a == 90) {
+        return true;
+    } else if (a == 91) {
+        return false;
+    } else if (a == 92) {
+        return true;
+    } else if (a == 93) {
+        return false;
+    } else if (a == 94) {
+        return true;
+    } else if (a == 95) {
+        return false;
+    } else if (a == 96) {
+        return true;
+    } else if (a == 97) {
+        return false;
+    } else if (a == 98) {
+        return true;
+    } else if (a == 99) {
+        return false;
+    } else if (a == 100) {
+        return true;
+    } else if (a == 101) {
+        return false;
+    } else if (a == 102) {
+        return true;
+    } else if (a == 103) {
+        return false;
+    } else if (a == 104) {
+        return true;
+    } else if (a == 105) {
+        return false;
+    } else if (a == 106) {
+        return true;
+    } else if (a == 107) {
+        return false;
+    } else if (a == 108) {
+        return true;
+    } else if (a == 109) {
+        return false;
+    } else if (a == 110) {
+        return true;
+    } else if (a == 111) {
+        return false;
+    } else if (a == 112) {
+        return true;
+    } else if (a == 113) {
+        return false;
+    } else if (a == 114) {
+        return true;
+    } else if (a == 115) {
+        return false;
+    } else if (a == 116) {
+        return true;
+    } else if (a == 117) {
+        return false;
+    } else if (a == 118) {
+        return true;
+    } else if (a == 119) {
+        return false;
+    } else if (a == 120) {
+        return true;
+    } else if (a == 121) {
+        return false;
+    } else if (a == 122) {
+        return true;
+    } else if (a == 123) {
+        return false;
+    } else if (a == 124) {
+        return true;
+    } else if (a == 125) {
+        return false;
+    } else if (a == 126) {
+        return true;
+    } else if (a == 127) {
+        return false;
+    } else if (a == 128) {
+        return true;
+    } else if (a == 129) {
+        return false;
+    } else if (a == 130) {
+        return true;
+    } else if (a == 131) {
+        return false;
+    } else if (a == 132) {
+        return true;
+    } else if (a == 133) {
+        return false;
+    } else if (a == 134) {
+        return true;
+    }
+}
